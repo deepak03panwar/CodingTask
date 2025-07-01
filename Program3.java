e1a798da6d69c2c0783106a90fe4d918c4922e95
deepak03panwar <142619765+deepak03panwar@users.noreply.github.com>
2025-07-01 11:17:39 +0530

    Add files via upload

--- Program3.java
+++ Program3.java
@@ -5,8 +5,8 @@ public class Program3 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        if(n % 2 == 0){
-            n = n-1;
+        if(n%2==0){
+            n=n-1;
         }
 
         for(int i=1;i<=n;i++){
